9efbdce73931f002c9cf0904cfb42878ef154139
dbstnqls6953@naver.com <dbstnqls6953@naver.com>
2022-02-03 22:10:46 +0900

    .

--- app.js
+++ app.js
@@ -17,13 +17,14 @@ const $result = document.querySelector("#result");
 
 // 계산기 숫자 클릭시 발생하는 함수 (e.target 사용ver)
 const onClickNumber = (e) => {
-  if(!operator){ // 연산자가 클릭되지 않은 경우(비어있다)
+  // 연산자가 클릭되지 않은 경우(비어있다)
+  if(!operator){ 
     numOne += e.target.innerText;
     $result.value += e.target.innerText;
     return;
     // return 다음에 오는 else 생략 가능
   }
-  // 비어있지 않다
+  // 연산자가 클릭된 경우
   if(!numTwo){ // numTwo가 없는 상황
     // 계산기 처음 실행할 때(numTwo를 처음 클릭할 때)에서는 화면에 보이는 숫자를 초기화
     $result.value = "";
@@ -56,13 +57,39 @@ document.querySelector("#num-9").addEventListener("click", onClickNumber);
 
 // e.target 사용ver
 const onClickOperator = (e) => {
-  if(numOne){
-    operator = e.target.innerText;
-  } 
-  else {
-    alert("숫자를 먼저 입력하세요!");
+  if(e.target.innerText === "-" && !numOne){
+    numOne += e.target.innerText;
+    $result.value += e.target.innerText;
+    // return;
   }
-  $operator.value = operator;
+  else {
+    if(numOne){
+      operator = e.target.innerText;
+      $operator.value = operator;
+    } else {
+      alert("숫자를 먼저 입력하세요!");
+    }
+    if(numTwo){
+      switch(operator){
+        case '+':
+          $result.value = parseInt(numOne) + parseInt(numTwo);
+          break;
+        case '-':
+          $result.value = parseInt(numOne) - parseInt(numTwo);
+          break;
+        case '/':
+          $result.value = parseInt(numOne) / parseInt(numTwo);
+          break;
+        case '*':
+          $result.value = parseInt(numOne) * parseInt(numTwo);
+          break;
+        default:
+          break;
+      }
+      numOne = $result.value;
+      numTwo = "";
+    }
+  }
 };
 
 document.querySelector("#plus").addEventListener("click", onClickOperator);
@@ -70,6 +97,52 @@ document.querySelector("#minus").addEventListener("click", onClickOperator);
 document.querySelector("#divide").addEventListener("click",onClickOperator);
 document.querySelector("#multiply").addEventListener("click",onClickOperator);
 
-document.querySelector("#calculate").addEventListener("click", () => {});
-document.querySelector("#clear").addEventListener("click", () => {});
+document.querySelector("#calculate").addEventListener("click", () => {
+  if(numTwo){
+    switch(operator){
+      case '+':
+        $result.value = parseInt(numOne) + parseInt(numTwo);
+        break;
+      case '-':
+        $result.value = parseInt(numOne) - parseInt(numTwo);
+        break;
+      case '/':
+        $result.value = parseInt(numOne) / parseInt(numTwo);
+        break;
+      case '*':
+        $result.value = parseInt(numOne) * parseInt(numTwo);
+        break;
+      default:
+        break;
+    }
+    $operator.value = "";
+    numOne = $result.value;
+    operator = "";
+    numTwo = "";
+  } else {
+    alert("숫자를 먼저 입력하세요!");
+  }
+});
 
+// swich문을 if문으로 바꿔보기
+// document.querySelector("#calculate").addEventListener("click", () => {
+//   if(!operator){ 
+//     alert("숫자를 먼저 입력하세요!");
+//   } else if(operator === "+"){
+//     $result.value = parseInt(numOne) + parseInt(numTwo);
+//   } else if(operator === "-"){
+//     $result.value = parseInt(numOne) - parseInt(numTwo);
+//   } else if(operator === "/"){
+//     $result.value = parseInt(numOne) / parseInt(numTwo);
+//   } else if(operator === "*"){
+//     $result.value = parseInt(numOne) * parseInt(numTwo);
+//   }
+// });
+
+document.querySelector("#clear").addEventListener("click", () => {
+  numOne = "";
+  operator = "";
+  numTwo = "";
+  $operator.value = "";
+  $result.value = "";
+});
